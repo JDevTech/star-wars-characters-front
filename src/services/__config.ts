5e0bbab8bcf1bc34f348333450aa7291ba06a528
Juan David Marcillo Alba <juan.marcillo@globant.com>
2021-12-21 10:52:46 -0500

    Adding production api url

--- src/services/__config.ts
+++ src/services/__config.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const ApiInstance = axios.create({
-  baseURL: "http://localhost:9000/api/",
+  baseURL: "https://star-wars-api-test.herokuapp.com/api/",
 });
 
 export default ApiInstance;
